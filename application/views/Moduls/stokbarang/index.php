7b7135ccd06697c3c2b20d61bdfb1a7a8f9e1d49
Luqman Hakim Nadzari <luqman_biru@yahoo.com>
2018-02-02 08:41:34 +0700

    Update model supplier

---
+++ application/views/Moduls/stokbarang/index.php
@@ -0,0 +1,157 @@
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1>
+                        Data Master Barang
+                        <small>List Data Master Barang</small>
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li><a href="<?php echo base_url(); ?>index.php/admin"><i class="fa fa-dashboard"></i> Home</a></li>
+                        <li class="active">List Data Master Barang</li>
+                    </ol>
+                </section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="box">
+                                <div class="box-header">
+                                    <!-- <h3 class="box-title">Data Table With Full Features</h3> -->                                    
+                                </div><!-- /.box-header -->
+                                <div class="box-body table-responsive">
+                                 <!-- <button class="btn btn-success" onclick="add_masterbrg()"><i class="glyphicon glyphicon-plus"></i> Master Barang</button> -->
+                                <br><br>
+                                    <table id="table" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Kode Barang</th>
+                                                <th>Nama Barang</th>
+                                                <th>Stok</th>
+                                                
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                        
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Kode Barang</th>
+                                                <th>Nama Barang</th>
+                                                <th>Stok</th>
+                                                
+                                                
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+                                </div><!-- /.box-body -->
+                            </div><!-- /.box -->
+                        </div>
+                    </div>
+                </section>
+
+<!-- SCRIPT -->
+<script type="text/javascript">
+ 
+var save_method; //for save method string
+var table;
+ 
+$(document).ready(function() {
+ 
+    //datatables
+    table = $('#table').DataTable({ 
+ 
+        "processing": true, //Feature control the processing indicator.
+        "serverSide": true, //Feature control DataTables' server-side processing mode.
+        "order": [], //Initial no order.
+ 
+        // Load data for the table's content from an Ajax source
+        "ajax": {
+            "url": "<?php echo site_url('admin/ajax_list_sb')?>",
+            "type": "POST"
+        },
+ 
+        //Set column definition initialisation properties.
+        "columnDefs": [
+        { 
+            "targets": [ -1 ], //last column
+            "orderable": false, //set not orderable
+        },
+        ],
+ 
+    });
+});
+ 
+function reload_table()
+{
+    //alert('TEST') ;
+    table.ajax.reload(null,false); //reload datatable ajax 
+}
+ 
+</script>
+ 
+<!-- Bootstrap modal -->
+<div class="modal fade" id="modal_form" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h3 class="modal-title">Master Barang Form</h3>
+            </div>
+            <div class="modal-body form">
+                <form action="#" id="form" class="form-horizontal">
+                    <input type="hidden" value="" name="id"/> 
+                    <div class="form-body">
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Kode Barang</label>
+                            <div class="col-md-9">
+                                <input name="KodeBarang" placeholder="" class="form-control" type="text" value="<?=$KdBrg?>" readonly>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Nama Barang</label>
+                            <div class="col-md-9">
+                                <input name="NamaBarang" placeholder="" class="form-control" type="text">
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Satuan</label>
+                            <div class="col-md-9">
+                                <select name="Satuan" class="form-control">
+                                    <option value="">--Select Satuan--</option>
+                                    <option value="PCS">PCS</option>
+                                    <option value="PACk">PACK</option>
+                                </select>
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Harga Pokok</label>
+                            <div class="col-md-9">
+                                <input name="HPP" placeholder="" class="form-control" type="text">
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-md-3">Harga Jual</label>
+                            <div class="col-md-9">
+                                <input name="HargaJual" placeholder="" class="form-control" type="text">
+                                <span class="help-block"></span>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" id="btnSave" onclick="save()" class="btn btn-primary">Save</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- End Bootstrap modal -->
+
+<!-- SCRIPT -->
